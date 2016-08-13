4692e81c46c4602193f993cfb92baeabfeb03e67
slx <lixin.shi@morningstar.com>
2016-08-12 21:25:45 -0400

    1

---
+++ src/main/java/com/arcturus/Logger.java
@@ -0,0 +1,35 @@
+package com.arcturus;
+
+public class Logger {
+
+	private Object obj = null;
+
+	private Logger(Object obj) {
+		setObj(obj);
+	}
+
+	public void info(Object msg) {
+	}
+
+	public void error(Object msg) {
+	}
+
+	public void warn(Object msg) {
+	}
+
+	public static Logger getLogger(Object obj) {
+		return new Logger(obj);
+	}
+
+	public void printToFile(String testCaseId, String msg) {
+		System.out.println(testCaseId + "\t" + msg);
+	}
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+}
